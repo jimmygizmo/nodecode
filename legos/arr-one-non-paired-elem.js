31af49a512a29987e809493b617eed565c683bd5
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2022-11-24 15:45:53 -0800

    legos/arr-one-non-paired-elem now creates needed hash of counts. next search for known count value of 1 and return its hash key (objects actually .. hash in the Python sense of dictionary etc.)

---
+++ legos/arr-one-non-paired-elem.js
@@ -0,0 +1,63 @@
+#! /usr/bin/env node
+
+'use strict';
+
+// const fs = require('fs');
+
+// process.stdin.resume();
+// process.stdin.setEncoding('utf-8');
+
+// let inputString = '';
+// let currentLine = 0;
+
+// process.stdin.on('data', function(inputStdin) {
+//     inputString += inputStdin;
+// });
+
+// process.stdin.on('end', function() {
+//     inputString = inputString.split('\n');
+
+//     main();
+// });
+
+// function readLine() {
+//     return inputString[currentLine++];
+// }
+main();
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function lonelyinteger(a) {
+    let counts = {};
+    a.map( elem => {
+        if ( typeof counts[elem] === 'undefined' ) {
+            counts[elem] = 1;
+        } else {
+            counts[elem] += 1;
+        }
+    })
+    console.log(counts);
+}
+
+function main() {
+    // const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    // const n = parseInt(readLine().trim(), 10);
+
+    // const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    // const result = lonelyinteger(a);
+
+    const result = lonelyinteger([1, 2, 3, 4, 3 ,2 ,1]);
+
+    console.log(result);
+
+    // ws.write(result + '\n');
+
+    // ws.end();
+}
